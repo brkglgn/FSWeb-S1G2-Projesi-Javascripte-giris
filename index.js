ca61a3bd9d8ef43abcf1983ff2aacf41547a48df
brkglgn <brkgulgen@hotmail.com>
2024-08-28 10:33:07 +0300

    ilkproje
    ilk görev yapıldı

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:
    3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
 */
 
-function carpma(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function carpma(sayi, sayi1) {
+  return sayi*sayi1
 }
 
 /* Görev 2 : Köpeğin Yaşı */
@@ -65,8 +65,8 @@ Aşağıdakileri yap:
  3. Hesaplanan köpeğin yaşını dönün.
  */
 
-function kopeginYasi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function kopeginYasi(yas) {
+  let kopek = yas/7
 }
 
 /* Görev 3 */
